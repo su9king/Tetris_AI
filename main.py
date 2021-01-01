fad9e27cdd3f4ef1d9473bdc333fdb81307fd876
MueTill <70988820+su9king@users.noreply.github.com>
2021-01-01 12:28:31 +0900

    Commit Complete

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import setting
 import Tetris_Option
+import pygame
+import random
+
+setting.set_window("test",500,500)
+
+while setting.program_run:
+
+    setting.get_event(pygame.event.get())
 
 
-program_run = True
 
-while program_run:
-
-    #setting.set_screen("test",500,500)
--- setting.py
+++ setting.py
@@ -1,10 +1,23 @@
 import pygame
 
+program_run = True
+
 #SCREEN OPTION
-def set_screen(title,width,height):
+def set_window(title,width,height):
 
     pygame.display.set_caption(title)
 
     screen = pygame.display.set_mode((width,height))
 
-    pygame.image.load(block_image)
+def get_event(event):
+
+    global program_run
+
+    for i in event:
+
+        if i.type == pygame.QUIT:
+
+            program_run = False
+
+
+
